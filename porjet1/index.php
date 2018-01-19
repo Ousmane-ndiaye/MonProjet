6730a838476dc5f3d4586544468a3c2223561d0c
Ousmanendiaye352 <ousmanendiaye352@gmail.com>
2018-01-19 19:52:40 +0000

    ousmane encore

--- porjet1/index.php
+++ porjet1/index.php
@@ -125,6 +125,9 @@
                 <span class="sr-only">Next</span>
             </a>
         </div>
+        <?php
+            //Ousmane Ndiaye 352 a fait ceci pour voir ce qui se passe
+        ?>
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
